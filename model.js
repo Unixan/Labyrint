96efaf679086afe6fcaf69872b4ff44aa9e4e7d2
Unixan <115973884+Unixan@users.noreply.github.com>
2023-01-09 12:42:55 +0100

    Start

---
+++ model.js
@@ -0,0 +1,42 @@
+let model = {
+  mazeModel: {
+    size: 2,
+    rows: [
+      [
+        { isHigh: false, isWide: false, isOpen: false },
+        { isHigh: false, isWide: true, isOpen: false },
+        { isHigh: false, isWide: false, isOpen: false },
+        { isHigh: false, isWide: true, isOpen: false },
+        { isHigh: false, isWide: false, isOpen: false },
+      ],
+      [
+        { isHigh: true, isWide: false, isOpen: false },
+        { isHigh: true, isWide: true },
+        { isHigh: true, isWide: false, isOpen: false },
+        { isHigh: true, isWide: true },
+        { isHigh: true, isWide: false, isOpen: false },
+      ],
+      [
+        { isHigh: false, isWide: false, isOpen: false },
+        { isHigh: false, isWide: true, isOpen: false },
+        { isHigh: false, isWide: false, isOpen: false },
+        { isHigh: false, isWide: true, isOpen: false },
+        { isHigh: false, isWide: false, isOpen: false },
+      ],
+      [
+        { isHigh: true, isWide: false, isOpen: false },
+        { isHigh: true, isWide: true },
+        { isHigh: true, isWide: false, isOpen: false },
+        { isHigh: true, isWide: true },
+        { isHigh: true, isWide: false, isOpen: false },
+      ],
+      [
+        { isHigh: false, isWide: false, isOpen: false },
+        { isHigh: false, isWide: true, isOpen: false },
+        { isHigh: false, isWide: false, isOpen: false },
+        { isHigh: false, isWide: true, isOpen: false },
+        { isHigh: false, isWide: false, isOpen: false },
+      ],
+    ],
+  },
+};
